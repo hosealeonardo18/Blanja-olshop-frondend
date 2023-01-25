58cbd0d452158095cf22d2dcd44e029486fe9173
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-01-25 18:54:27 +0700

    banyak dah revisinya

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,38 +80,3 @@ const swiper = new Swiper(".slide-content", {
 //   },
 // });
 
-
-// sbadmin
-if ('WebSocket' in window) {
-  (function () {
-    function refreshCSS() {
-      const sheets = [].slice.call(document.getElementsByTagName('link'));
-      const head = document.getElementsByTagName('head')[0];
-      for (const i = 0; i < sheets.length; ++i) {
-        const elem = sheets[i];
-        const parent = elem.parentElement || head;
-        parent.removeChild(elem);
-        const rel = elem.rel;
-        if ((elem.href && typeof rel != 'string') || rel.length == 0 || rel.toLowerCase() == 'stylesheet') {
-          const url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
-          elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + new Date().valueOf();
-        }
-        parent.appendChild(elem);
-      }
-    }
-    const protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
-    const address = protocol + window.location.host + window.location.pathname + '/ws';
-    const socket = new WebSocket(address);
-    socket.onmessage = function (msg) {
-      if (msg.data == 'reload') window.location.reload();
-      else if (msg.data == 'refreshcss') refreshCSS();
-    };
-    if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
-      console.log('Live reload enabled.');
-      sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
-    }
-  })();
-} else {
-  console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
-}
-// ]]>
